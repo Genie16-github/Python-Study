6e34954b314fc41b0930fa5a5acf378499ce94c4
JaeBeom <80519614+Genie16-github@users.noreply.github.com>
2022-08-31 18:39:41 +0900

    [S4]스위치 켜고 끄기

--- baekjoon_/1244.py
+++ baekjoon_/1244.py
@@ -1,5 +1,5 @@
 # [S4]스위치 켜고 끄기
-def OnOff(num):
+def OnOff(num):  # 스위치 조작 함수
     if data[num] == 1:
         data[num] = 0
     else:
@@ -7,21 +7,21 @@ def OnOff(num):
 
 n = int(input())
 data = list(map(int, input().split()))
-data = [0] + data
+data = [0] + data  # 스위치 번호 1번부터 시작
 
 for _ in range(int(input())):
     sex, num = map(int, input().split())
 
-    if sex == 1:
+    if sex == 1:  # 남자
         for i in range(num, len(data), num):
             OnOff(i)
-    else:
+    else:  # 여자
         OnOff(num)
-        for i in range(n//2):
+        for i in range(n//2):  # 하나를 기준으로 양쪽으로 체크하기 때문에 최대 n//2번
             if num - i < 1 or num + i > n:
                 break
 
-            if data[num - i] != data[num + i]:
+            if data[num - i] != data[num + i]:  # 대칭되는 스위치의 값이 다르면 탈출
                 break
             else:
                 OnOff(num - i)
@@ -29,5 +29,5 @@ for _ in range(int(input())):
 
 for i in range(1, len(data)):
     print(data[i], end=" ")
-    if i % 20 == 0:
+    if i % 20 == 0:  # 20개 초과시 한줄 밑에 출력
         print()
