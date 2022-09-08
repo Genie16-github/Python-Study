45cba1d63b7431fa935ef2a5e94bd619e0e569bf
JaeBeom <80519614+Genie16-github@users.noreply.github.com>
2022-09-08 23:21:48 +0900

    update

--- BaekJoon/14490.py
+++ BaekJoon/14490.py
@@ -2,9 +2,9 @@
 def gcd(a, b):
     if b == 0:
         return a
-    return gcd(b, a%b)
+    return gcd(b, a % b)
 
 n, m = map(int, input().split(':'))
 g = gcd(max(n, m), min(n, m))
-result=f'{n//g}:{m//g}'
+result = f'{n//g}:{m//g}'
 print(result)
