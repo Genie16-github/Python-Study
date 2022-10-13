cd1a28b9afe076f110fae7d9b7f65511830f9e0d
JaeBeom <4411dlwoqja@naver.com>
2022-10-14 02:40:34 +0900

    update file

---
+++ BaekJoon/2839.py
@@ -0,0 +1,23 @@
+# [S4]설탕 배달
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+if n % 5 == 0:
+    print(n // 5)
+else:
+    cnt = 0
+    while n > 0:
+        n -= 3
+        cnt += 1
+        if n % 5 == 0:
+            cnt += n // 5
+            print(cnt)
+            break
+        elif n == 0:
+            print(cnt)
+            break
+        elif n == 1 or n == 2:
+            print(-1)
+            break
