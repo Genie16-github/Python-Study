4101b16975da7cffb27344ccd7b5d334a6f87a8e
JaeBeom <4411dlwoqja@naver.com>
2022-11-05 02:48:47 +0900

    update

--- BaekJoon/10709.py
+++ BaekJoon/10709.py
@@ -5,22 +5,22 @@ input = sys.stdin.readline
 H, W = map(int, input().split())
 board = [input() for _ in range(H)]
 cloud = False
-board2 = [[-1]*W for _ in range(H)]
+res = [[-1]*W for _ in range(H)]
 
 for i in range(H):
     for j in range(W):
         if board[i][j] == 'c':
             cloud = True
-            board2[i][j] = 0
+            res[i][j] = 0
             continue
 
         if cloud:
-            board2[i][j] = board2[i][j-1] + 1
+            res[i][j] = res[i][j-1] + 1
 
     cloud = False
 
 for i in range(H):
-    print(*board2[i])
+    print(*res[i])
 
 
 
