8068b5c2bf988da6e977bd48f7f5c8661028c96d
JaeBeom <4411dlwoqja@naver.com>
2022-11-09 18:24:00 +0900

    update file

--- Python/s4/s4_4.py
+++ Python/s4/s4_4.py
@@ -1,1 +1,27 @@
 # 마구간 정하기(결정 알고리즘)
+def Count(x):
+    cnt = 1
+    ep = x_arr[0]
+    for i in range(1, len(x_arr)):
+        if x_arr[i] - ep >= x:
+            cnt += 1
+            ep = x_arr[i]
+
+    return cnt
+
+
+N, C = map(int, input().split())
+x_arr = [int(input()) for _ in range(N)]
+x_arr.sort()
+res = 0
+lp = 1  # 두 마구간의 거리는 최소 1
+rp = x_arr[N-1]  # 마지막 마구간까지의 거리
+while lp <= rp:
+    md = (lp + rp) // 2
+    if Count(md) >= C:
+        res = md
+        lp = md + 1
+    else:
+        rp = md - 1
+
+print(res)
---
+++ Python/s4/s4_5.py
@@ -0,0 +1,12 @@
+# 회의실 배정(그리디)
+N = int(input())
+time = [list(map(int, input().split())) for _ in range(N)]
+time.sort(key=lambda x: [x[1], x[0]])
+
+cnt = 0
+et = 0
+for i in range(len(time)):
+    if time[i][0] >= et:
+        cnt += 1
+        et = time[i][1]
+print(cnt)
